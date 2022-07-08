984ad723a16b93e4a7a157c176e0dcee03851e6e
esg-emperor <66887093+esg-emperor@users.noreply.github.com>
2022-07-08 17:27:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 import json
 
 list_keywords = ['climate','environment','waste','renewable','water', 'carbon',
-           'sustainab','strategy','scope','emission','ghg', 'co2',
+           'strategy','scope','emission','ghg', 'co2',
            'greenhouse','target', 'environment', 
             
             'board overs','board overs', 'committ',
@@ -28,7 +28,7 @@ list_keywords = ['climate','environment','waste','renewable','water', 'carbon',
             
             'power','electric','green','energy','air',
             'catastroph','physical','transition','solar','coal','net-zero','net zero', 'catastroph',
-            'hurricane','terrorism','storm','tornado','severe','weather','IPCC','°','warm','land','ocean'
+            'hurricane','terrorism','storm','tornado','severe','weather','IPCC','warm','land','ocean'
             
             'ghg', 'emission', 'greenhouse', 'co2', 'scope 1', 'scope 2', 'scope 3', 'carbon',
             
@@ -175,11 +175,47 @@ Note that the *Keyword diversity* slider only works if the *MMR* checkbox is tic
 
 
     data="""
-    	The Company also considers climate change risks in making investment decisions to acquire and develop properties.  As part of our proactive measures to increase awareness and preparedness for the potential future impact from climate change, the Company completed resilience assessments of our Boston portfolio and our Los Angeles portfolio.  Through these assessments, we have developed a strategy and methodology for assessing climate resilience that will be expanded in 2022 to our full portfolio and potential investment opportunities.  In addition, the Company partners with cities and local governments, through organizations like the Boston Green Ribbon Commission, to develop climate resilience plans and strategies for fighting climate change.  We aim to make climate resilience a key driver in our overall business strategy in order to mitigate risks and identify long-term value creation opportunities. 
- 	Through this screening process, we weigh several sustainability characteristics that contribute to long-term value and resilience.  We conduct energy audits and identify potential opportunities to increase efficiency in building systems.  This includes considering LEED status, on-site clean and renewable energy, energy intensity, benchmarking our energy use and scanning for lighting retrofits and central system controls.  We also consider physical risks such as the potential for flooding, wildfires and environmental hazards and conduct a Phase I Environmental Site Assessment on all new acquisitions. 
- 	2 Equity Residential used the Intergovernmental Panel on Climate Change (IPCC) Representative Concentration Pathway (RCP) 4. 5 and a “businesses-as usual” scenario of RCP 8. 5 as the two scenarios to assess impacts and selected medium- and longer-term timeframes of 2030 and 2050 based on the types of expected hazards and regulatory frameworks impacting the Boston area. 
- 	Environment—Climate Strategy and Portfolio Resilience;Sustainable Buildings;Energy and Emissions Describe the resilience of the organizations strategy, taking into consideration different climate-related scenarios, including a 2°C or lower scenario.
-	Deutsche Bank has signed up to the World Green Building Council’s (WGBC) Net Zero Carbon Buildings Commitment, pledging to reduce and compensate operational emissions associated with energy used to light, heat, cool and power buildings, for assets over which it has direct control.
+    	
+	
+	The Travelers Companies, Inc. (NYSE: TRV) today published its 2021 Sustainability Report, which outlines the company’s comprehensive strategy for creating shareholder value over time, as well as its ongoing environmental, social and governance (ESG) initiatives.
+“Our long-term success is tied to our ability to execute in the marketplace while upholding the promise we made to be there for our customers, communities and employees,” said Yafit Cohn, Chief Sustainability Officer at Travelers. “This year’s sustainability report details that approach and highlights how our ESG initiatives are integrated throughout everything we do.”
+The report provides an in-depth review of the 16 topics the company has identified as its key drivers of sustained value: Business Strategy & Competitive Advantages, Capital & Risk Management, Climate Strategy, Community, Customer Experience, Data Privacy & Cybersecurity, Disaster Preparedness & Response, Diversity & Inclusion, Eco-Efficient Operations, Ethics & Values, Governance Practices, Human Capital Management, Innovation, Investment Management, Public Policy and Safety & Health.
+Highlights from the company’s 2021 report include:
+
+Climate Strategy
+
+    Over the past three years, the company’s Global Renewable Energy Practice grew at a compound annual rate of 30%, with revenue up more than 120% since 2018.
+
+Community
+
+    Travelers and the Travelers Foundation have contributed more than $228 million to nonprofits over the past decade to support academic and career success, develop thriving neighborhoods and create culturally enriched communities.
+
+Customer Experience
+
+    The company resolved more than 90% of property claims arising out of catastrophe events within 30 days. This result was achieved while responding to a historically high number of catastrophe events in 2021.
+
+Diversity & Inclusion
+
+    In each of the last 10 years, Travelers has increased the percentage of people of color in its U.S. workforce while increasing the percentage of both women and people of color in U.S. management-level positions.
+
+Eco-Efficient Operations
+
+    Over the past decade, the company reduced its Scope 1 and Scope 2 emissions by 60%, and in 2021, Travelers announced its commitment to become carbon neutral across its owned operations by 2030.
+
+Human Capital Management
+
+    Continuing to focus on the health and wellness of its employees, Travelers introduced a new benefit – Caregiving Support from Wellthy – to help employees navigate challenges they may face as caregivers for children, aging family members or loved ones with special needs or who are chronically ill.
+
+Investment Management
+
+    Many of the company’s investments enable environmental and social improvements, including water and sewer projects ($6 billion), K-12 education ($10.8 billion) and higher education ($5 billion). Travelers also owns nearly $2.4 billion in fixed income securities classified as “green bonds” by Bloomberg.
+
+Public Policy
+
+    Throughout 2020 and 2021, the Travelers Institute hosted 45 virtual events to help consumers and business leaders navigate through uncertain times. The programs, which explored topics like cybersecurity, distracted driving and natural disasters, garnered nearly 100,000 views.
+
+The company’s sustainability reporting is guided by multiple sustainability disclosure frameworks, as well as engagements with key stakeholders from both inside and outside the company. Travelers is a leader among domestic property casualty insurance companies in publishing reports responsive to the Sustainability Accounting Standards Board (SASB) standards for the insurance industry and the recommendations of the Task Force on Climate-related Financial Disclosures (TCFD). 
+	
 	""",
 	
 
